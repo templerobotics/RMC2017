84f40819da0ba99dfa690e1696dce9dccb9d11e7
swilso421 <swilso421@gmail.com>
2016-11-30 10:13:33 -0500

    Update formattingTools.py

--- Comms/formattingTools.py
+++ Comms/formattingTools.py
@@ -1,9 +1,13 @@
+#!/usr/bin/env python
+
+#Converts a dictionary to a string in the form 'key1:value1;key2:value2;'
 def toString(dictionary):
     string = ''
     for key in dictionary:
         string += '{}:{};'.format(key, str(dictionary[key]))
     return string
 
+#Converts a string of the form 'key1:value1;key2:value2;' to a dictionary of string keys and float values
 def toDict(string):
     dictionary = {}
     pairs = string.split(';')
