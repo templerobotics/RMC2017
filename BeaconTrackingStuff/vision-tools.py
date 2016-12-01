177359aa4567bf42eada829a6d3053a7721881e9
swilso421 <swilso421@gmail.com>
2016-11-30 22:16:09 -0500

    Update vision-tools.py

--- BeaconTrackingStuff/vision-tools.py
+++ BeaconTrackingStuff/vision-tools.py
@@ -2,29 +2,52 @@
 
 import cv2
 
+#Simplified grayscale function, assumes input is BGR
 def toGray(image):
     return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
+#Simplified gaussian blur, not to be confused with cv2.blur()
 def blur(image, ksize = 5, sigmax = 0, sigmay = 0):
     return cv2.GaussianBlur(image, (ksize, ksize), sigmax, sigmay)
 
+#Simplified threshold function, assumes maxVal to be 255
 def threshold(image, threshold, thresholdType = cv2.THRESH_BINARY):
-    return cv2.threshold(image, threshold, 255, thresholdType)
+    _, threshed = cv2.threshold(image, threshold, 255, thresholdType)
+    return threshed
 
+#Simplified edge detection, uses Canny edge detection
+def edgeDetect(image, preBlurred = False):
+    if image.channels() > 1:
+        image = toGray(image)
+    if not preBlurred:
+        image = blur(image)
+    image = cv2.Canny(image, 50, 150)
+    return image
+
+#Simplified findContours, assumes RETR_EXTERNAL and CHAIN_APPROX_SIMPLE
 def getContours(image):
     contours, _ = cv2.findContours(image.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     return contours
 
-def countVertices(contour):
-    peri = cv2.arcLength(c, True)
-    approx = cv2.approxPolyDP(c, 0.04 * peri, True)
-    return len(approx)
-
+#Used on binary images to remove blobs too big to be removed by blurring
 def removeSmallWhiteBlobs(image, aggressiveness = 4):
     image = cv2.erode(image, None, iterations = aggressiveness)
     image = cv2.dilate(image, None, iterations = aggressiveness)
     return image
 
+#Used on binary images to remove blobs too big to be removed by blurring
+def removeSmallBlackBlobs(image, aggressiveness = 4):
+    image = cv2.dilate(image, None, iterations = aggressiveness)
+    image = cv2.erode(image, None, iterations = aggressiveness)
+    return image
+
+#Counts the approximate number of vertices of a contour
+def countVertices(contour):
+    peri = cv2.arcLength(c, True)
+    approx = cv2.approxPolyDP(c, 0.04 * peri, True)
+    return len(approx)
+
+#Classifies a contour as a generic polygon shape
 def identifyShape(contour, circleLimit = 10):
     nVertices = countVertices(contour)
     shape = 'circle'
@@ -51,4 +74,3 @@ def identifyShape(contour, circleLimit = 10):
     elif nVertices == 10:
         shape = 'decagon'
     return shape
-
