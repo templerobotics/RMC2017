8487f421b897801b4eb9fe7ab202ad872ccebc6c
swilso421 <swilso421@gmail.com>
2017-02-06 08:26:25 -0500

    Update of formattingTools to preserve data type

--- Comms/formattingTools.py
+++ Comms/formattingTools.py
@@ -3,8 +3,25 @@
 #Converts a dictionary to a string in the form 'key1:value1;key2:value2;'
 def toString(dictionary):
     string = ''
+    typeCode = ''
     for key in dictionary:
-        string += '{}:{};'.format(key, str(dictionary[key]))
+        valueType = type(dictionary[key])
+
+        if valueType is int:
+            typeCode = 'i'
+        elif valueType is float:
+            typeCode = 'f'
+        elif valueType is long:
+            typeCode = 'l'
+        elif valueType is bool:
+            typeCode = 'b'
+        elif valueType is None:
+            typeCode = 'n'
+        else:
+            typeCode = 's'
+
+        string += '{}:{}:{};'.format(key, typeCode, str(dictionary[key]))
+
     return string
 
 #Converts a string of the form 'key1:value1;key2:value2;' to a dictionary of string keys and float values
@@ -14,5 +31,19 @@ def toDict(string):
     del pairs[-1]
     for word in pairs:
         keyValue = word.split(':')
-        dictionary[keyValue[0]] = float(keyValue[1])
+        typeCode = keyValue[1]
+
+        if typeCode == 'i':
+            dictionary[keyValue[0]] = int(keyValue[2])
+        elif typeCode == 'f':
+            dictionary[keyValue[0]] = float(keyValue[2])
+        elif typeCode == 'l':
+            dictionary[keyValue[0]] = long(keyValue[2])
+        elif typeCode == 'b':
+            dictionary[keyValue[0]] = bool(keyValue[2])
+        elif typeCode == 'n':
+            dictionary[keyValue[0]] = None
+        else:
+            dictionary[keyValue[0]] = keyValue[2]
+            
     return dictionary
