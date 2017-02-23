ac2bb1bf1b072ee4cca52bfeaacde9462d3eaca5
BrianAmin <tuf49787@temple.edu>
2017-02-23 13:56:46 -0500

    Added comments and cleaned up base code

--- MotorControlLibaries/Nerf/fullfrontal.py
+++ MotorControlLibaries/Nerf/fullfrontal.py
@@ -10,34 +10,34 @@ from socket import *
 #Motor Functions
 
 
-def getArea(rectangle):
-    return rectangle[2] * rectangle[3]
 
 
-
-
+#Load frame, apply front face haar classifier, and check to see if identified rectangle is in center of frame/center of gun
 def processframe():
+    #grab frame
     retval, frame = cam.read()
     #frame = cv2.imread('img.png')
     #frame = camera.GrabNumPyImage(format='bgr')
     
     #frame = cv2.resize(frame,None,fx=0.5, fy=0.5, interpolation = cv2.INTER_AREA)
+    #Init Haar Frontal Face Classifier
     detect = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-    upper = cv2.CascadeClassifier('haarcascade_frontalface_alt2.xml')
-	#comments
+    #Alternative Classifier:
+    #upper = cv2.CascadeClassifier('haarcascade_frontalface_alt2.xml')
+    #Apply the frontal classifier:
     frontal = detect.detectMultiScale(frame,scaleFactor=1.3, minNeighbors=5, minSize=(30, 30), flags=cv2.CASCADE_SCALE_IMAGE)
-    #Eventually will download and implement profile classifier
-    profile = upper.detectMultiScale(frame,scaleFactor=1.3, minNeighbors=5, minSize=(30, 30), flags=cv2.CASCADE_SCALE_IMAGE)
 
-    
+    #frontal is an array full of rectangle coords regarding identified objects
     for (x,y,w,h) in frontal:
         
-        
+        #center of rectangle, draw a circle
         center = ( (x+(w/2)), (y+(h/2)))
         cv2.rectangle(frame, (x, y), (x+w, y+h), (0,0,153), 5)	
         cv2.circle(frame, (center), 3, (0, 200, 0), -1)
+        #Grab center of rectangle (only x is needed) and test to see if it falls within
         center = ( (x+(w/2)))
 		#print(center)
+        #Send pysocket commands to motorcontroller/pi regarding situation
         if (center < 390 and center > 250 ):
             cv2.putText(frame, "Target Found", (20, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.5,(0, 0, 255), 2)
             clientSocket.sendto('shoot'.encode(),serverAddress)
@@ -49,35 +49,29 @@ def processframe():
 
 
         
-#context = pyfly2.Context()
-#if context.num_cameras:
-#    camera = context.get_camera(0)
-#    camera.Connect()
-#    camera.StartCapture()
 
+#Pi/Motor Controller's IP and Port
 targetIP = '192.168.1.102'
 port = 12000
 
+
+#Init socket
 clientSocket = socket(AF_INET,SOCK_DGRAM)
 serverAddress = (targetIP, port)
 
 
 
-STATE_TARGET_FOUND = False
-STATE_PANNING = False
-DEBUG_MODE = True
-currentPanAngle = 90
 
 
 
-
-
+#OpenCV int webcam
 port = int(raw_input("Camera Port: "))
 cam = cv2.VideoCapture(port)
 
 cam.release()
 cam.open(port)
 time.sleep(5)
+#Initial command to get gun moving
 clientSocket.sendto('right'.encode(),serverAddress)
 
 
