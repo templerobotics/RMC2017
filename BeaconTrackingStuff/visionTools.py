724ee1f5f5b5a63caf363f77def1bfbbd3c615e6
Sam Wilson <swilso421@gmail.com>
2017-04-13 21:20:55 -0400

    Updated files related to vision

--- BeaconTrackingStuff/visionTools.py
+++ BeaconTrackingStuff/visionTools.py
@@ -2,7 +2,9 @@
 #Sam Wilson 1/25/2017
 #See Sam for questions
 import cv2
+import sys
 import globalConstants as gc
+import numpy as np
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
 
@@ -32,9 +34,37 @@ def edgeDetect(image, preBlurred = False):
 
 #Simplified findContours, assumes RETR_EXTERNAL and CHAIN_APPROX_SIMPLE
 def getContours(image):
+    if len(image[0][0]) > 1:
+        image = toGray(image)
     contours, _ = cv2.findContours(image.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     return contours
 
+#Returns the largest contour from a given set
+def getLargestContour(contours):
+    largestA = -1.0
+    try:
+        largestC = contours[0]
+    except:
+        return None
+    for c in contours:
+        area = cv2.contourArea(c)
+        if area > largestA:
+            largestA = area
+            largestC = c
+    return largestC
+
+#Calculates the center of a contour
+def findCenter(contour):
+    M = cv2.moments(contour)
+    x = int(M['m10'] / M['m00'])
+    y = int(M['m01'] / M['m00'])
+    return (x, y)
+
+#Calculates angle by using the focal length and pixel position
+def getAnglesToPoint(point, camMatrix):
+    yaw = 90 - math.degrees(math.atan((point[0] - 319) / camMatrix[0, 0]))
+    #pitch = 90 - math.degrees(math.atan(point[1] - camMatrix[1, 2]) / camMatrix[1, 1]))
+    return (yaw, 0)
 
 #============================Custom Tools to Make Life Easier=================================
 
@@ -84,6 +114,29 @@ def identifyShape(contour, circleLimit = 10):
         shape = 'decagon'
     return shape
 
+#Returns a binary mask highlighting zones matching specified hsv bounds
+def getColorThresholdMask(image, hue, hueMargin = 20, satLimit = 100, valLimit = 30, swapBR = False):
+    lowerFilter = np.array([hue - hueMargin, satLimit, valLimit], dtype = np.uint8)
+    upperFilter = np.array([hue + hueMargin, 255, 255], dtype = np.uint8)
+    if swapBR:
+        hsv = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
+    else:
+        hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    mask = cv2.inRange(hsv, lowerFilter, upperFilter)
+    return mask
+
+#Finds the largest contour matching the specified color
+#Current color options are 'green' and 'red'
+def findColoredContour(image, color):
+    if color == 'green':
+        mask = getColorThresholdMask(image, 50, hueMargin = 10, satLimit = 50)
+    elif color == 'red':
+        mask = getColorThresholdMask(image, 127, hueMargin = 8, satLimit = 120)
+    thresholded = cv2.bitwise_and(image, image, mask = mask)
+    contour = getLargestContour(getContours(thresholded))
+    return contour
+        
+
 #==========================Tools for making OpenCV and ROS work together======================
 
 #Converts sensor_msgs.msg/Image to OpenCV Mat
@@ -94,33 +147,52 @@ def ImgMsg2Mat(img_msg):
 def Mat2ImgMsg(mat):
     return cv2_to_imgmsg(mat, encoding = 'passthrough')
 
+#=========================================Math Tools==========================================
+
+#Given two legs of a triangle and a base, calculates the length of the base's median
+#Negative returns indicate errors
+def medianFormula( leg_m, leg_n, base):
+    #1/2 * sqrt(2*m^2 + 2*n^2 - k^2)
+    try:
+        return 0.5 * math.sqrt(2 * leg_m ** 2 + 2 * leg_n ** 2 - base ** 2)
+    except:
+        sys.stderr.write("WARNING: Complex value of median")
+        sys.stderr.write("leg_m = " + str(leg_m))
+        sys.stderr.write("leg_n = " + str(leg_n))
+        sys.stderr.write("base = " + str(base))
+        return -1
+
 #==========================================Classes============================================
 
-#Make sure to release or delete this object so that other scripts can use the cameras
-class RobotCameras():
-    def __init__(self):
-        self._sCam = cv2.VideoCapture(gc.STAR_CAMERA_INDEX)
-        self._pCam = cv2.VideoCapture(gc.PORT_CAMERA_INDEX)
+#Wrapper for cv2's VideoCapture which automatically undistorts the images it captures
+class Camera():
+    def __init__(self, index, cameraMatrix, distortionConstants):
+        self.cam = cv2.VideoCapture(index)
+        self.matrix = cameraMatrix
+        self.distortion = distortionConstants
 
     def __del__(self):
-        self._sCam.release()
-        self._pCam.release()
+        self.cam.release()
 
-    def getStarImage(self):
-        sImage = self._sCam.grab()
-        sImage = self._sCam.retrieve(sImage)
-        return sImage
+    def _getRawImage(self):
+        image = self.cam.grab()
+        image = self.cam.retrieve(image)
+        return image
 
-    def getPortImage(self):
-        pImage = self._pCam.grab()
-        pImage = self._pCam.retrieve(pImage)
-        return pImage
-
-    def getImages(self):
-        sImage = self.getStarImage()
-        pImage = self.getPortImage()
-        return sImage, pImage
+    def getImage(self):
+        image = self._getRawImage()
+        image = cv2.undistort(image, self.matrix, self.distortion)
+        return image
 
     def release(self):
-        self._sCam.release()
-        self._pCam.release()
+        self.cam.release()
+
+#Wrapper for Ted
+class Ted(Camera):
+    def __init__(self):
+        super(Ted, self).__init__(gc.TED_INDEX, gc.TED_MATRIX, gc.TED_DISTORTION)
+
+#Wrapper for Bill
+class Bill(Camera):
+    def __init__(self):
+        super(Bill, self).__init__(gc.BILL_INDEX, gc.BILL_MATRIX, gc.BILL_DISTORTION)
