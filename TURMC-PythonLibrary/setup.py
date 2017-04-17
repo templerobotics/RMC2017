b893589538511fa8bb60d8cbfa5c50acdfed5826
swilso421 <swilso421@gmail.com>
2017-04-17 10:02:08 -0400

    Updated to include PySerial dependency

--- TURMC-PythonLibrary/setup.py
+++ TURMC-PythonLibrary/setup.py
@@ -3,10 +3,10 @@
 from setuptools import setup, find_packages
 
 setup(name="TURMC-PythonLibrary",
-      version="1.0.5",
+      version="1.0.7",
       description="A library of code used by Temple Robotic's RMC team",
       author="Brian Amin, Sam Wilson",
       url="www.github.com/TURMC1617/main",
       packages=find_packages(),
-      install_requires=['numpy', 'matplotlib', 'scipy', 'pigpio']
+      install_requires=['numpy', 'matplotlib', 'scipy', 'pigpio', 'pyserial']
      )
