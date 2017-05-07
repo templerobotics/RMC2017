f40dab3b106b605d805228f3d8ba28e0d26f1567
Sam Wilson <swilso421@gmail.com>
2017-05-06 22:21:17 -0400

    Added stepper control and image sending

--- Comms/TeleopNuc.py
+++ Comms/TeleopNuc.py
@@ -1,5 +1,6 @@
-from turmc.global_constants import NUC_PORT
+from turmc.global_constants import *
 from turmc.networking.joystick import Server
+from turmc.networking.image_comm import ImageSender
 from turmc.motor_control.camera_stepper import CameraStepper
 
 #The 'justSent' booleans make the images only get sent on presses, and prevents
@@ -8,8 +9,29 @@ justSentUseless = False
 justSentBill = False
 justSentTed = False
 
+#Initialize camera objects
+TED = cv2.VideoCapture(TED_INDEX)
+BILL = cv2.VideoCapture(BILL_INDEX)
+USELESS = cv2.VideoCapture(USELESS_INDEX)
+
 #Initialize the camera stepper object
 stepper = CameraStepper()
+
+#Creates an ImageSender object pointing at the base computer
+imageSender = ImageSender(BASE_COMPUTER_IP, BASE_COMPUTER_PORT)
+
+def sendImage(cameraName, grayscale = True, metadata = {}):
+    if cameraName == 'Useless':
+        frame = USELESS.read()
+    elif cameraName == 'Bill':
+        frame = BILL.read()
+    elif cameraName == 'Ted':
+        frame = TED.read()
+    else:
+        return
+    if grayscale:
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    imageSender.sendImage(frame, cameraName, metadata)
 
 def handle(data):
     #Sends an image from Useless
@@ -28,12 +50,18 @@ def handle(data):
 
     #Sends an image from Ted
     if data['button12'] == 1.0 and not justSentTed:
-        sendImage('Ted')
+        sendImage('Ted', metadata = {'position': stepper.getPosition(), 'bearing': stepper.getBearing()})
         justSentTed = True
     elif data['button12'] == 0.0 and justSentTed:
         justSentTed = False
 
-    #TODO: Control the CameraStepper using the left and rights movements of the thumbstick
+    #Camera stepper controls; will hold position if not moving
+    if data['hatX'] == -1:
+        stepper.stepLeft()
+    elif data['hatX'] == 1:
+        stepper.stepRight()
+    else:
+        stepper.stop()
 
 #Starts up a server listening on the Nuc port
 def main():
