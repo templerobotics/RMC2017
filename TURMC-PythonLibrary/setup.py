e28b5c818076bbc4e09a4e2ad22a92022953435b
swilso421 <swilso421@gmail.com>
2017-07-25 13:57:05 -0400

    Update setup.py

--- TURMC-PythonLibrary/setup.py
+++ TURMC-PythonLibrary/setup.py
@@ -3,10 +3,10 @@
 from setuptools import setup, find_packages
 
 setup(name="TURMC-PythonLibrary",
-      version="1.0.12",
-      description="A library of code used by Temple Robotic's RMC team",
+      version="1.0.14",
+      description="A library of code used by Temple Robotics team",
       author="Brian Amin, Sam Wilson",
-      url="www.github.com/TURMC1617/main",
+      url="www.github.com/templerobotics/main",
       packages=find_packages(),
       #install_requires=['numpy', 'matplotlib', 'pigpio', 'pyserial'],
       #provides=['Phidgets']
